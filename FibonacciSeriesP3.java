2634aeb4292a951fd62c975cddf6318efb4c21d0
Anjaliydv9598 <anjaliydv9598.com>
2026-05-11 12:36:13 +0530

    WAJP to print a Fibonacci Series

--- FibonacciSeriesP3.java
+++ FibonacciSeriesP3.java
@@ -6,7 +6,7 @@ public class FibonacciSeriesP3 {
 	public static void main(String[] args) {
 		int a= 0,b=1;
 		System.out.print(a+" "+b);
-		for(int i=1;i<=6;i++) {
+		for(int i=1;i<=7;i++) {
 			int c= a+b;
 			System.out.print(" "+c);
 			a=b;b=c;
