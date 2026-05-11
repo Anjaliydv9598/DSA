0fb3a1217b164a5b3400bd58f603f273a5cf57c4
Anjaliydv9598 <anjaliydv9598.com>
2026-05-11 12:40:29 +0530

    WAJP to print a Fibonacci Series

--- FibonacciSeries.java
+++ FibonacciSeries.java
@@ -1,9 +1,11 @@
 package com.dsa.practice;
 
 public class FibonacciSeries {
-
+	
+	//WAJP to print a Fibonacci Series
+	
 	public static void main(String[] args) {
-		 int n=10;
+		 int n=9;
 		 int a=0 , b=1;
 		 System.out.print(a+" "+b);
 		 for(int i=2 ; i<=n ;i++ )
