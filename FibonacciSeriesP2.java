555dd2ba2b1d8f0eab5110793fdfb015a808d23c
Anjaliydv9598 <anjaliydv9598.com>
2026-05-11 12:38:46 +0530

    WAJP to print a fibonacci series

--- FibonacciSeriesP2.java
+++ FibonacciSeriesP2.java
@@ -3,7 +3,7 @@ package com.dsa.practice;
 public class FibonacciSeriesP2 {
 
 	public static void main(String[] args) {
-		int n=5;
+		int n=6;
 		int a=0,b=1;
 		System.out.print(a+" "+b);
 		for(int i=2;i<=n;i++) {
