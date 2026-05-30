1db8d363d11b4e042bf88117410f147240a94a65
Anjaliydv9598 <anjaliydv9598.com>
2026-05-30 13:41:26 +0530

    WAJP Execution of Main method in Stack area

---
+++ BasicExecution.java
@@ -0,0 +1,26 @@
+package com.dsa.practice;
+
+public class BasicExecution {
+	
+	//Execution of Main method in Stack area
+
+		public static void main(String[] args) {
+			System.out.println("main start");
+			m1();
+			System.out.println("main end");
+		}
+		
+		public static void m1() {
+			m3();
+			System.out.println("m1 called");
+			m2();
+		}
+		
+		public static void m2() {
+			System.out.println("m2 called");
+		}
+		
+		public static void m3() {
+			System.out.println("m3 called");
+		}
+}
