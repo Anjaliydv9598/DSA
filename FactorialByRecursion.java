216d7f3fd41cd4461f67b9e3b7813cc794a085ff
Anjaliydv9598 <anjaliydv9598@gmail.com>
2026-06-01 14:05:26 +0530

    WAJP to print Factorial of 5 using Recursion

--- FactorialByRecursion.java
+++ FactorialByRecursion.java
@@ -5,13 +5,14 @@ public class FactorialByRecursion {
 //		WAJP Print factorial of 5 using recursion
 		public static void main(String[] args) {
 			int fact = print(5);
-			System.out.print(fact);
+			System.out.print("Factorial : " + fact);
 		}
 		
 		public static int print(int n) {
 			if(n==1) {
 				return 1;		
 			}
+			
 			return n*print(n-1);
 			
 		}
