8d74d0e36b6eebf0961953e4335c0ba3d9169f39
Anjaliydv9598 <anjaliydv9598@gmail.com>
2026-06-02 13:32:04 +0530

    Sum Of Natural number using Recursion

--- SumOfNatNum_Recursion.java
+++ SumOfNatNum_Recursion.java
@@ -2,7 +2,7 @@ package com.dsa.practice;
 
 public class SumOfNatNum_Recursion {
 
-//	Print Sum of 3 natural no. using recursion
+//  WAJP Print Sum of 5,7 natural no. using recursion
 	
 	public static int print(int n) {
 		if(n==1) {
@@ -11,8 +11,22 @@ public class SumOfNatNum_Recursion {
 		return n+print(n-1);
 	}
 	
+	public static int num(int n) {
+		if(n==0) {
+			return 0;
+		}
+		return n + num(n-1);
+	}
+	
 	
 	public static void main(String [] args) {
+		System.out.println("Print sum of 5 natural no. using recursion");
 		int sum=print(5);
-		System.out.println(sum);	}
+		System.out.println(sum);
+//		------------------------
+		System.out.println();
+		System.out.println("Print sum of 7 natural no. using recursion");
+		int summ=num(7);
+		System.out.println(summ);
+	}
 }
