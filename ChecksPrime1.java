6d3363317d65491cb246f6d07666f995af3c8532
Anjaliydv9598 <anjaliydv9598@gmail.com>
2026-06-05 13:36:27 +0530

    WAJP to checks prime or not using Java8

---
+++ ChecksPrime1.java
@@ -0,0 +1,17 @@
+package com.dsa.practice;
+
+import java.util.stream.IntStream;
+
+public class ChecksPrime1 {
+	
+	public static boolean isPrime(int num) {
+		return num>1 &&
+				IntStream.rangeClosed(2, (int)Math.sqrt(num))
+				.noneMatch(i->num%i==0);
+	}
+	
+	public static void main(String [] args) {
+		System.out.println(isPrime(17));
+	}
+
+}
