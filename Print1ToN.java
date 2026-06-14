151739cd89c8cc6bd011f4dd089816e12495496b
Anjaliydv9598 <anjaliydv9598@gmail.com>
2026-06-14 23:12:50 +0530

    WAJP to Print numbers from 1 to N using recursion

---
+++ Print1ToN.java
@@ -0,0 +1,18 @@
+package com.dsa.practice;
+
+public class Print1ToN {
+
+    public static void printNumbers(int n) {
+        if (n == 0) {
+            return;
+        }
+
+        printNumbers(n - 1);
+        System.out.print(n + " ");
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        printNumbers(n);
+    }
+}
