eb56d6d2726173a4a4ed6ab719db53d20a971b86
Anjaliydv9598 <anjaliydv9598@gmail.com>
2026-06-15 16:57:40 +0530

    WAJP to Built-in-Reverse

---
+++ ReverseAString1.java
@@ -0,0 +1,15 @@
+package com.dsa.practice;
+
+public class ReverseAString1 {
+	
+	public static void main(String [] args) {
+		String str = "hello";
+		String reversed = "";
+		
+		for(int i=str.length()-1 ; i>=0 ; i--) {
+			reversed += str.charAt(i);
+		}
+		System.out.println(reversed);
+	}
+
+}
