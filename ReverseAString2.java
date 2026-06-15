4e1d5f21d530a5e1ca08e6da79d5834fee356489
Anjaliydv9598 <anjaliydv9598@gmail.com>
2026-06-15 17:03:19 +0530

    WAJP to reverse a String Concatination

---
+++ ReverseAString2.java
@@ -0,0 +1,15 @@
+package com.dsa.practice;
+
+public class ReverseAString2 {
+	
+	public static void main(String [] args) {
+		String str="REPOLEVED ERAWTFOS";
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i = str.length()-1 ; i>=0 ; i--) {
+			sb.append(str.charAt(i));
+		}
+		System.out.println(sb.toString());
+	}
+
+}
