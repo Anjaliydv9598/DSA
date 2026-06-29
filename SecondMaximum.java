711c2525b259d1fb98846584b0d62c13835b5cfa
Anjaliydv9598 <anjaliydv9598@gmail.com>
2026-06-29 11:23:17 +0530

    WAJP to find the 2nd Maximum from an Array

---
+++ SecondMaximum.java
@@ -0,0 +1,28 @@
+package com.dsa.practice;
+
+public class SecondMaximum {
+	
+	public static int secondMax(int [] arr) {
+		
+		int maximum= arr[0];
+		int secondMaximum=arr[1];
+		
+		for(int i=2 ; i<arr.length; i++) {
+			if(arr[i]>maximum) {
+				secondMaximum=maximum;
+				maximum=arr[i];
+			}
+			else if(arr[i]>secondMaximum) {
+				secondMaximum=arr[i];
+			}
+		}
+		return secondMaximum;
+		
+	}
+	
+	public static void main(String[] args) {
+		int [] arr= {1,2,3,4,5,6};
+		System.out.println(secondMax(arr));
+	}
+
+}
