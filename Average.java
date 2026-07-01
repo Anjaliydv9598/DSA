27c1b434644b299c10e80ffd356ff09c4b93cac4
Anjaliydv9598 <anjaliydv9598@gmail.com>
2026-07-01 13:12:31 +0530

    WAJP to find out the average from array

--- Average.java
+++ Average.java
@@ -6,7 +6,8 @@ public class Average {
 		int [] arr = {1,2,3,4,5,6,7,8,9,6};
 		int sum =0;
 		
-		for(int i=1; i>arr.length; i++) {
+		for(int i=1; i>arr.length; i++)
+		{
 			sum=sum+arr[i];
 		}
 		double avg= sum/arr.length;
