76d22f10b62a04f2a4fbb0888ff6bf9b8aa99609
Anjaliydv9598 <anjaliydv9598@gmail.com>
2026-07-14 13:58:10 +0530

    WAJP to find frequency of all the caharcters

---
+++ FrequencyChar.java
@@ -0,0 +1,25 @@
+package com.dsa.practice;
+
+public class FrequencyChar {
+	
+	public static void main(String[] args) {
+		String str= "qwertyuiop asfghjkl zxcvbnm";
+		
+		int [] count = new int [256];		// for all ASCII 
+		
+		//count frequency
+		for(int i=0; i<str.length();i++) {
+			count [str.charAt(i)]++;
+		}
+		
+		//print frequency
+		for(int i=0;i<str.length();i++) {
+			if(count[str.charAt(i)] != 0) {
+				System.out.println(str.charAt(i) + " = " + count[str.charAt(i)]);
+				
+				count[str.charAt(i)]=0;			//avoid duplicate
+			}
+		}
+	}
+
+}
