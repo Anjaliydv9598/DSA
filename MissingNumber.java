f105f0e3a86fdb1baba3f7708e7369647571bf96
Anjaliydv9598 <anjaliydv9598@gmail.com>
2026-07-15 11:47:04 +0530

    WAJP rotate the array k times

--- MissingNumber.java
+++ MissingNumber.java
@@ -4,8 +4,10 @@ public class MissingNumber {
 
 	public static void main(String[] args) {
 		int [] arr = {1,2,4,5};
+		
 		int n=arr.length+1;
-		int totalSum = n+(n+1)/2;
+		
+		int totalSum = n*(n+1)/2;
 		
 		int arrSum=0;
 		
---
+++ RotateArray.java
@@ -0,0 +1,25 @@
+package com.dsa.practice;
+
+public class RotateArray {
+	
+	public static void main(String[] args) {
+		
+		int [] arr = {1,2,3,4,5};
+		int k = 2;
+		
+		for(int i=0; i<k; i++) {
+			int last = arr[arr.length-1];
+			
+			//shift night
+			for(int j=arr.length-1; j>0; j--) {
+				arr [j]=arr[j-1];
+			}
+			arr[0]=last;
+		}
+		
+		for(int num : arr) {
+			System.out.println(num + " ");
+		}
+	}
+
+}
