9d01b4e8607f934326af1c50fab022ee382f4378
Anjaliydv9598 <anjaliydv9598@gmail.com>
2026-07-17 23:43:38 +0530

    WAJP for swapping

---
+++ Swap.java
@@ -0,0 +1,19 @@
+package com.dsa.practice;
+
+public class Swap {
+	
+	public static void main(String[] args) {
+		
+		int a=5;
+		int b=10;
+		int temp;;
+		
+		temp=a;
+		a=b;
+		b=temp;
+		
+		System.out.println();
+		System.out.println("The elements after swapping :-  " + "a=" + a + " b=" + b);
+	}
+
+}
