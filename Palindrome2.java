aa33a5b7d297f2bf3bd4219e346dd0f1bc7956e4
Anjaliydv9598 <anjaliydv9598@gmail.com>
2026-07-23 11:48:42 +0530

    WAJP to check the given String is palindrome or not

--- Palindrome2.java
+++ Palindrome2.java
@@ -1,5 +1,23 @@
 package com.dsa.practice;
 
 public class Palindrome2 {
+	
+	public static void main(String[] args) {
+		String str = "Madam";
+		str=str.toLowerCase();
+	
+		String reversed="";
+		
+		for(int i=str.length()-1; i>=0; i--) {
+			reversed+=str.charAt(i);
+		}
+		
+		if(str.equals(reversed)) {
+			System.out.println("Palindrome");
+		}
+		else {
+			System.out.println("Not Palindrome");
+		}
+	}
 
 }
