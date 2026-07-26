d5c0d6cd7e3b3838d603f345d69f550a030c9ac7
Anjaliydv9598 <anjaliydv9598@gmail.com>
2026-07-27 00:24:47 +0530

    WAJP to Find First Non-Repeating Character using java8

--- NonRepeativeChar.java
+++ NonRepeativeChar.java
@@ -7,22 +7,22 @@ import java.util.stream.Collectors;
 
 public class NonRepeativeChar {
 	  public static void main(String[] args) {
-	        String input = "aabccdeff";
+		  String input = "aabccdeff";
 
-	        Character result = input.chars()
-	                .mapToObj(c -> (char) c)
-	                .collect(Collectors.groupingBy(
-	                        Function.identity(),
-	                        LinkedHashMap::new,
-	                        Collectors.counting()
-	                ))
-	                .entrySet()
-	                .stream()
-	                .filter(entry -> entry.getValue() == 1)
-	                .map(Map.Entry::getKey)
-	                .findFirst()
-	                .orElse(null);
+		  Character result = input.chars()                 // take each letter
+		      .mapToObj(c -> (char) c)                     // convert to char
+		      .collect(Collectors.groupingBy(              // count letters
+		          Function.identity(),
+		          LinkedHashMap::new,
+		          Collectors.counting()
+		      ))
+		      .entrySet()
+		      .stream()
+		      .filter(e -> e.getValue() == 1)              // keep only count = 1
+		      .map(e -> e.getKey())                        // get character
+		      .findFirst()                                 // first one
+		      .orElse(null);
 
-	        System.out.println(result);
+		  System.out.println(result);
 	    }
 }
