bb8dc8b1d7da1a8cf83826330bcf360bbfdf0e1f
Anjaliydv9598 <anjaliydv9598@gmail.com>
2026-08-23 23:15:53 +0530

    WAJP for equilateral triangle

--- PatternPyramid.java
+++ PatternPyramid.java
@@ -4,9 +4,11 @@ public class PatternPyramid {
 
 	public static void main(String[] args) {
 		
-		for(int i=1; i<=5; i++) {
+		int n=5;
+		
+		for(int i=1; i<=n; i++) {
 			//print space
-			for(int j=1; j<=5-i; j++) {
+			for(int j=1; j<=n-i; j++) {
 				System.out.print(" ");
 			}
 			//print stars
