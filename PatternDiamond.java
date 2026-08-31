7119b4700b49fa1a5189bfd1a08779531c0e3ed5
Anjaliydv9598 <anjaliydv9598@gmail.com>
2026-08-31 20:39:34 +0530

    Practice

--- PatternDiamond.java
+++ PatternDiamond.java
@@ -2,4 +2,39 @@ package com.dsa.practice;
 
 public class PatternDiamond {
 
+	  public static void main(String[] args) {
+
+	        int n = 5;
+
+	        // Upper Pyramid
+	        for (int i = 1; i <= n; i++) {
+
+	            // Spaces
+	            for (int j = 1; j <= n - i; j++) {
+	                System.out.print(" ");
+	            }
+
+	            // Stars
+	            for (int j = 1; j <= 2 * i - 1; j++) {
+	                System.out.print("*");
+	            }
+
+	            System.out.println();
+	        }
+	        // Lower Inverted Pyramid
+	        for (int i = n - 1; i >= 1; i--) {
+
+	            // Spaces
+	            for (int j = 1; j <= n - i; j++) {
+	                System.out.print(" ");
+	            }
+	            
+	            // Stars
+	            for (int j = 1; j <= 2 * i - 1; j++) {
+	                System.out.print("*");
+	            }
+	            System.out.println();
+	            
+	        }
+	    }
 }
